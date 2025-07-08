7460c110ebd30c3545932cf61b16830674ac8345
otsairaze <ssavelevdenchikk@gmail.com>
2025-07-08 22:22:33 +0300

    chore: add CI/CD pipeline configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import { FlatCompat } from "@eslint/eslintrc";
-import importPlugin from "eslint-plugin-import";
 import prettierPlugin from "eslint-plugin-prettier";
+import storybook from "eslint-plugin-storybook";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,45 +12,42 @@ const compat = new FlatCompat({
   baseDirectory: __dirname
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
-  plugins: {
-    prettier: prettierPlugin,
-    import: importPlugin
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "dist/**"]
   },
-  settings: {
-    "import/resolver": {
-      typescript: {}
+  {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {}
+      }
+    },
+    rules: {
+      "max-len": ["error", { code: 120, ignoreComments: true }],
+      "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".tsx"] }],
+      "react/function-component-definition": [2, { namedComponents: "arrow-function" }],
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 120,
+          endOfLine: "auto"
+        }
+      ],
+      "import/no-unresolved": "off",
+      "import/extensions": "off"
     }
   },
-  rules: {
-    "import/order": [
-      "error",
-      {
-        groups: ["builtin", "external", "internal"],
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true
-        }
-      }
-    ],
-    "max-len": ["error", { code: 120, ignoreComments: true }],
-    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".tsx"] }],
-    "react/function-component-definition": [2, { namedComponents: "arrow-function" }],
-    "react/react-in-jsx-scope": "off",
-    "no-unused-vars": "warn",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 120,
-        endOfLine: "auto"
-      }
-    ],
-    "import/no-unresolved": "off",
-    "import/extensions": "off"
-  }
-}, ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"]];
+  ...storybook.configs["flat/recommended"],
+  ...storybook.configs["flat/recommended"]
+];
 
 export default eslintConfig;
